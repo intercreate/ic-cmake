6f4cc4f74ea6aec3dd31a4c01393c4635af1e1c8
J.P. Hutchins <jphutchins@gmail.com>
2023-10-31 15:13:31 -0700

    fix: support <= 3.24

--- fetch_ic.cmake
+++ fetch_ic.cmake
@@ -5,11 +5,15 @@
 
 include_guard(GLOBAL)
 
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.24.0")
+    set(download_extract_timestamp "DOWNLOAD_EXTRACT_TIMESTAMP true")
+endif()
+
 include(FetchContent)
 
 FetchContent_Declare(ic_cmake
     URL https://github.com/intercreate/test-cmake/releases/latest/download/ic.zip
-    DOWNLOAD_EXTRACT_TIMESTAMP true
+    ${download_extract_timestamp}
 )
 FetchContent_MakeAvailable(ic_cmake)
 
